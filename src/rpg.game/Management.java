05ba9f9e7e60c9a349de547e89fad32bacb86300
Maghwyn <christopher.samson@edu.esiee-it.fr>
2021-11-26 08:18:16 +0100

    rework

--- src/rpg.game/Management.java
+++ src/rpg.game/Management.java
@@ -1,2 +1,35 @@
-package rpg.game;public class Interface {
+package rpg.game;
+
+interface Management {
+    void add_character(Character new_character);
+    void removeCharacter(int characterID);
+    int doesCharacterExist(int characterID);
+    void display_list();
+}
+
+interface Stats {
+    String getClassName();
+    String getCharacterName();
+    int getHealthPoints();
+    int getAttackDamages();
+    int getInitiative();
+
+    int getShield();
+    int getEnhancedDamages();
+    float getAgility();
+    float getCriticalChance();
+    boolean isCritical();
+    void disableCritical();
+    void enableCritical();
+}
+
+interface Fighting {
+    void takeDamages(int damages);
+    Character getFighter(int index);
+}
+
+interface Save {
+    String IndexType();
+    String convertToJSON();
+    String toString();
 }
